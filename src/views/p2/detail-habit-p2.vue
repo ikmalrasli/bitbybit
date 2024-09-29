a12b60a7b4d9aa2d8d3a9df8d44623917bb746ce
eemauuu <ikmalrasli@gmail.com>
2024-09-29 18:51:30 +0800

    added plus minus buttons, slider doesn't go below current progress, createEntry button disabled if new progress not greater than current progress

--- src/views/p2/detail-habit-p2.vue
+++ src/views/p2/detail-habit-p2.vue
@@ -4,23 +4,30 @@
       <header class="bg-white p-4 flex flex-row relative justify-between">
         <button @click="goBack" class="material-icons">chevron_left</button>
         <h1 class="text-lg text-black font-semibold">{{ selectedHabit?.name || 'Habit Details' }}</h1>
-        <button @click="createEntry" class="material-icons">check</button>
+        <button @click="createEntry" class="material-icons disabled:opacity-50" :disabled="addProgress === selectedHabit.progress">check</button>
       </header>
 
       <div class="flex-1 overflow-y-auto px-4">
-        <div class="w-full p-4 mb-8 text-gray-700 bg-white shadow-md rounded-xl text-center">
+        <div class="w-full p-4 mb-8 text-gray-700 bg-white border rounded-lg text-center">
             <h2 class="flex-auto text-xl block mb-2">Progress</h2>
             <h1 class="flex-auto text-5xl">{{ addProgress }}</h1>
             
             <h2 class="flex-auto text-xl mb-2">/ {{ selectedHabit?.dailyGoal }}</h2>
+            <!-- Minus Button -->
+            <button type="button" @click="decreaseGoal" class="bg-gray-300 text-gray-700 min-w-8 min-h-8 rounded-md">-</button>
+
+            <!-- Slider -->
             <input v-if="selectedHabit" 
             type="range" :min="0" :max="selectedHabit.dailyGoal" 
             v-model="addProgress"
-            class="range accent-violet-400 w-2/5" >
+            class="range accent-violet-400 w-2/5 mx-2" >
+
+            <!-- Plus Button -->
+            <button type="button" @click="increaseGoal" class="bg-gray-300 text-gray-700 min-w-8 min-h-8 rounded-md">+</button>
             
         </div>
 
-        <div class="w-full p-4 mb-4 text-gray-700 bg-white shadow-lg rounded-xl">
+        <div class="w-full p-4 mb-4 text-gray-700 bg-white border rounded-lg">
           <h2 class="text-xl text-center block">Notes</h2>          
           <p class="text-lg px-4 py-2">{{ selectedHabit?.notes }}</p>
           <div class="px-4 py-2 bg-green-300 text-lg text-center h-64 rounded-md">Add image here</div>
@@ -45,15 +52,37 @@ export default {
   },
   data() {
     return {
-      addProgress : 0,
+      addProgress: 0,
     };
   },
   computed: {
     ...mapState(['selectedHabit']),
+    //if selectedHabit changes, reset addProgress
+    selectedHabit() {
+      this.addProgress = this.$store.state.selectedHabit.progress;
+      return this.$store.state.selectedHabit;
+    }
   },
+  watch: {
+    addProgress(newVal) {
+      if (newVal < this.selectedHabit.progress) {
+        this.addProgress = this.selectedHabit.progress;
+      }
+    }
+  },  
   methods: {
     goBack() {
       this.$router.push('/');
+    },
+    increaseGoal() {
+      if (this.addProgress < this.selectedHabit.dailyGoal) {
+        this.addProgress++;
+      }
+    },
+    decreaseGoal() {
+      if (this.addProgress > this.selectedHabit.progress) {
+        this.addProgress--;
+      }
     },
     async createEntry() {
       try {
@@ -64,15 +93,14 @@ export default {
           throw new Error("User not authenticated. Please log in.");
         }
 
-        if (this.addProgress <= this.selectedHabit.progress){
-          alert("New progress cannot be less than current progress.");
+        if (this.addProgress <= this.selectedHabit.progress) {
           throw new Error("New progress cannot be less than current progress."); 
         }
 
         const habitRef = await addDoc(collection(db, "progress"), {
-          habitId : this.selectedHabit.habitId,
-          progress : Number(this.addProgress),
-          timestamp : new Date(),
+          habitId: this.selectedHabit.habitId,
+          progress: Number(this.addProgress),
+          timestamp: new Date(),
         });
 
         console.log("Progress created successfully! Habit name:", this.selectedHabit.name);
@@ -80,10 +108,9 @@ export default {
         this.goBack(); // Navigate back after successful creation
       } catch (e) {
         console.error("Error adding document: ", e);
-        alert("An error occurred while creating the progress. Please try again.");
       }
     }
   }
 }
 </script>
-  
+
