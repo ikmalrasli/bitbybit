133d039f2360750c6547637cfdb0ac2f8da23944
eemauuu <ikmalrasli@gmail.com>
2024-10-17 12:42:55 +0800

    UI fix, overflow and button

--- src/views/p2/detail-sunnah-p2.vue
+++ src/views/p2/detail-sunnah-p2.vue
@@ -4,21 +4,24 @@
       <!-- Header -->
       <header class="bg-white p-4 flex flex-row relative">
         <button @click="goBack" class="material-icons">chevron_left</button>
-        <h1 class="text-lg text-black font-semibold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{{sunnah.name}}</h1>
+        <h1 class="text-lg text-black font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">{{sunnah.name}}</h1>
       </header>
-      <p class="mt-2 p-4">{{ sunnah.description }}</p>
 
+      <!-- Content -->
+      <p class="mt-2 p-4 overflow-y-auto h-full" style="white-space: pre-wrap;">{{ sunnah.description }}</p>
+
+      <!-- Create Button -->
+      <div class="sticky bottom-0 p-4">
+        <button v-if="sunnah" @click="addToHabits" class="w-full bg-violet-400 text-white font-bold py-3 rounded-lg shadow-lg">
+          Add to habits 
+        </button>
+      </div>
     </div>
     <div v-else>
       <p>Loading...</p>
     </div>
 
-    <!-- Floating Create Button -->
-    <div class="sticky bottom-0 p-4">
-      <button v-if="sunnah" @click="addToHabits" class="w-full bg-violet-400 text-white font-bold py-3 rounded-lg shadow-lg">
-        Add to habits 
-      </button>
-    </div>
+    
   </div>
 </template>
 
