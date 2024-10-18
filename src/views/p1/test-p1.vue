810e0e93419eca6e9b91a547b328c216b6186461
eemauuu <ikmalrasli@gmail.com>
2024-10-18 18:34:21 +0800

    tested pinia and toast feature

--- src/views/p1/test-p1.vue
+++ src/views/p1/test-p1.vue
@@ -1,36 +1,44 @@
 <template>
   <div class="p-4 w-full">
-    <h1>User: {{ username }}</h1>
+    <h1 class="text-center">User: {{ username }}</h1>
+    <div class="my-4 border p-4 text-center">
+    <p>Count: {{ counterStore.count }}</p>
+    <p>Double Count: {{ counterStore.doubleCount }}</p>
+    <button class="bg-violet-400 text-white font-bold p-2 rounded-lg shadow-lg active:bg-violet-500" 
+      @click="counterStore.increment">Increase counter</button>
+    </div>
+    
+    <div class="border border-blue-500 p-2 rounded-lg flex space-x-2">
+      <button class="flex-1 bg-green-400 text-white font-bold p-2 rounded-lg shadow-lg active:bg-green-500" 
+      @click="showSuccessToast">Show Success Toast</button>
+      <button class="flex-1 bg-red-400 text-white font-bold p-2 rounded-lg shadow-lg active:bg-red-500" 
+      @click="showErrorToast">Show Error Toast</button>
+    </div>
   </div>
 </template>
 
 <script>
-import VerticalProgressbar from '../../components/VerticalProgressbar.vue';
-import HomeProgress from '../../components/home-progressbar.vue';
-import RadialProgressbar from '../../components/RadialProgressbar.vue';
+import { useCounterStore } from '../../store/counterStore';
 
 export default {
-  components: {
-    VerticalProgressbar,
-    HomeProgress,
-    RadialProgressbar
-  },
   data() {
     return {
+      counterStore: useCounterStore(), // Access the store directly
       username: this.$store.state.user.displayName,
-      hello : false
-    }
+    };
   },
   methods: {
-    checkUser() {
-      this.$store.state.weekHabits.forEach(element => {
-        console.log(element)
-      })
+    showSuccessToast() {
+      this.$toast.success({
+        message: 'This is a success message!',
+        duration: 1000
+      });
+    },
+    showErrorToast() {
+      this.$toast.error({
+        message: 'This is an error message!'
+      });
     },
   },
-}
+};
 </script>
-
-<style>
-
-</style>
