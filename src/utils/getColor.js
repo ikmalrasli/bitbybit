7e29f296718e2c64061b83f4f8155d962fb28908
eemauuu <ikmalrasli@gmail.com>
2024-10-19 23:45:58 +0800

    to manipulate color strings for tailwind css

---
+++ src/utils/getColor.js
@@ -0,0 +1,23 @@
+export function getBgColor(color) {
+    return 'bg-' +color.default
+}
+
+export function getActiveColor(color) {
+    return 'bg-' + color.active
+}
+
+export function getTextColor(color) {
+    return 'text-' + color.default
+}
+
+export function getBorderColor(color) {
+    return 'border-' + color.active
+}
+
+export function getAccentColor(color) {
+    return 'accent-' + color.default
+}
+
+export function getRingColor(color) {
+    return 'ring-' + color.active
+}
