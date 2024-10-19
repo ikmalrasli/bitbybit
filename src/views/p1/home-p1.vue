5f653901ff784373bf895dfe6695c247b4b393d5
eemauuu <ikmalrasli@gmail.com>
2024-10-19 23:36:13 +0800

    attempt to add animation to expand/collapse habits. (only collapse works)

--- src/views/p1/home-p1.vue
+++ src/views/p1/home-p1.vue
@@ -1,80 +1,74 @@
 <template>
   <div class="w-full flex flex-row px-4 py-2">
     <div class="flex-auto">
-        <!-- if no habits-->
-        <div v-if="uncompletedHabits.length===0 && completedHabits.length===0" class="w-full p-4 mb-4 text-gray-700">
-          <p class="text-center">No Habits</p>
-        </div>
+      <!-- if no habits-->
+      <div v-if="uncompletedHabits.length === 0 && completedHabits.length === 0" class="w-full p-4 mb-4 text-gray-700">
+        <p class="text-center">No Habits</p>
+      </div>
 
-        <!-- Collapsible Section -->
-        <div class="mt-1 space-y-4">
-          <!-- Uncompleted habits -->
-          <div v-if="uncompletedHabits.length !== 0">
-            <div class="flex items-center justify-between cursor-pointer" @click="toggleSection('Uncompleted')">
-              <div class="flex items-center">
-                <span class="font-semibold text-black">Uncompleted</span>
-                <span class="pl-3 text-gray-500">{{ uncompletedHabits.length }}</span>
-              </div>
-
-              <!-- Horizontal Line -->
-              <hr class="flex-grow border-t border-gray-300 mx-4"/>
-
-              <!-- Arrow -->
-              <div class="flex items-center">
-                <span v-if="showUncompleted" class="material-icons">keyboard_arrow_up</span>
-                <span v-else class="material-icons">keyboard_arrow_down</span>
-              </div>
+      <!-- Collapsible Section -->
+      <div class="mt-1 space-y-4">
+        <!-- Uncompleted habits (expand/collapse) -->
+        <div v-if="uncompletedHabits.length !== 0">
+          <div class="flex items-center justify-between cursor-pointer" @click="toggleSection('Uncompleted')">
+            <div class="flex items-center">
+              <span class="font-semibold text-black">Uncompleted</span>
+              <span class="pl-3 text-gray-500">{{ uncompletedHabits.length }}</span>
             </div>
+            <hr class="flex-grow border-t border-gray-300 mx-4" />
+            <div class="flex items-center">
+              <span v-if="showUncompleted" class="material-icons">keyboard_arrow_up</span>
+              <span v-else class="material-icons">keyboard_arrow_down</span>
+            </div>
+          </div>
 
-            <!-- Uncompleted habits List -->
-            <div v-if="uncompletedHabits.length!==0" v-show="showUncompleted" class="py-4 space-y-1 transition-all duration-300 ease-in-out">
+          <!-- Uncompleted habits List with Transition -->
+          <transition name="slide-fade" appear>
+            <div v-if="showUncompleted" class="py-4 space-y-1">
               <div v-for="(habit, index) in uncompletedHabits" :key="index">
                 <HomeProgress 
                   :percent="habit.progress * 100 / habit.dailyGoal"
                   :text="habit.name"
                   :timesdone="habit.progress + '/' + habit.dailyGoal"
-                  :color="habit.color ? habit.color.default : 'bg-violet-400'"
+                  :color="habit.color ? getBgColor(habit.color) : 'bg-violet-400'"
                   class="cursor-pointer"
                   @click="openDetail(habit)"
                 />
               </div>
             </div>
+          </transition>
+        </div>
+
+        <!-- Completed habits (expand/collapse) -->
+        <div v-if="completedHabits.length !== 0">
+          <div class="flex items-center justify-between cursor-pointer" @click="toggleSection('Completed')">
+            <div class="flex items-center">
+              <span class="font-semibold text-black">Completed</span>
+              <span class="pl-3 text-gray-500">{{ completedHabits.length }}</span>
+            </div>
+            <hr class="flex-grow border-t border-gray-300 mx-4" />
+            <div class="flex items-center">
+              <span v-if="showCompleted" class="material-icons">keyboard_arrow_up</span>
+              <span v-else class="material-icons">keyboard_arrow_down</span>
+            </div>
           </div>
 
-          <!-- Completed habits -->
-          <div v-if="completedHabits.length !== 0">
-            <div class="flex items-center justify-between cursor-pointer" @click="toggleSection('Completed')">
-              <div class="flex items-center">
-                <span class="font-semibold text-black">Completed</span>
-                <span class="pl-3 text-gray-500">{{ completedHabits.length }}</span>
-              </div>
-
-              <!-- Horizontal Line -->
-              <hr class="flex-grow border-t border-gray-300 mx-4"/>
-
-              <!-- Arrow -->
-              <div class="flex items-center">
-                <span v-if="showCompleted" class="material-icons">keyboard_arrow_up</span>
-                <span v-else class="material-icons">keyboard_arrow_down</span>
-              </div>
-            </div>
-
-
-            <!-- Uncompleted habits List -->
-            <div v-if="completedHabits.length!==0" v-show="showCompleted" class="py-4 transition-all duration-300 ease-in-out">
+          <!-- Completed habits List with Transition -->
+          <transition name="slide-fade" appear>
+            <div v-if="showCompleted" class="py-4">
               <div v-for="(habit, index) in completedHabits" :key="index" class="mb-2">
                 <HomeProgress 
                   :percent="habit.progress * 100 / habit.dailyGoal"
                   :text="habit.name"
                   :timesdone="habit.progress + '/' + habit.dailyGoal"
-                  :color="habit.color ? habit.color.default : 'bg-violet-400'"
+                  :color="habit.color ? getBgColor(habit.color) : 'bg-violet-400'"
                   class="cursor-pointer"
                   @click="openDetail(habit)"
                 />
               </div>
             </div>
-          </div>
-        
+          </transition>
+        </div>
       </div>
     </div>
   </div>
@@ -84,6 +78,7 @@
 import { mapState } from 'vuex';
 import calendarRow from "../../components/calendar-row.vue";
 import HomeProgress from "../../components/home-progressbar.vue";
+import { getBgColor, getActiveColor } from '../../utils/getColor';
 
 export default {
   components: {
@@ -111,6 +106,7 @@ export default {
     }
   },
   methods: {
+    getBgColor, getActiveColor,
     toggleSection(section) {
       if (section === 'Uncompleted') {
         this.showUncompleted = !this.showUncompleted;
@@ -141,3 +137,13 @@ export default {
   }
 };
 </script>
+
+<style>
+.slide-fade-enter-active, .slide-fade-leave-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-enter, .slide-fade-leave-to {
+  transform: translateY(-10px);
+  opacity: 0;
+}
+</style>
