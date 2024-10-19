c857cbdee2fe9f979f5809e25e99ba7bcd6ca49c
eemauuu <ikmalrasli@gmail.com>
2024-10-19 23:46:21 +0800

    for dynamic color tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,19 @@ export default {
     extend: {},
   },
   plugins: [],
+  safelist: [
+    {pattern: /^bg-(red|green|blue|yellow|pink|violet)-400$/},
+    {pattern: /^bg-(red|green|blue|yellow|pink|violet)-500$/,
+      variants: ['hover', 'active'],
+    },
+    {pattern: /^border-(red|green|blue|yellow|pink|violet)-500$/},
+    {pattern: /^border-(red|green|blue|yellow|pink|violet)-500$/,
+      variants: ['focus']
+    },
+    {pattern: /^ring-(red|green|blue|yellow|pink|violet)-500$/,
+      variants: ['focus', 'focus-within']
+    },
+    {pattern: /^text-(red|green|blue|yellow|pink|violet)-400$/},
+    {pattern: /^accent-(red|green|blue|yellow|pink|violet)-400$/},
+  ]
 }
-
