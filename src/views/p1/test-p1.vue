2c5f7fde33cd3422c6ff380abda28a4b0563f935
eemauuu <ikmalrasli@gmail.com>
2024-11-03 01:00:08 +0800

    testing

--- src/views/p1/test-p1.vue
+++ src/views/p1/test-p1.vue
@@ -14,17 +14,37 @@
       <button class="flex-1 bg-red-400 text-white font-bold p-2 rounded-lg shadow-lg active:bg-red-500" 
       @click="showErrorToast">Show Error Toast</button>
     </div>
+
+    <habitPb
+    :color="'bg-green-300'"
+    :text="'hello world'"
+    :percent="100"
+    class="my-4"/>
+
+    <habitPb
+    :color="'bg-emerald-300'"
+    :text="'hello world'"
+    :percent="100"
+    class="my-4"/>
   </div>
 </template>
 
 <script>
 import { useCounterStore } from '../../store/counterStore';
+import { useStatStore } from '../../store/statStore';
+import habitPb from '../../components/habitpb.vue';
 
 export default {
+  components: {
+    habitPb
+  },
   data() {
     return {
       counterStore: useCounterStore(), // Access the store directly
       username: this.$store.state.user.displayName,
+      color: { default: "bg-blue-400", active: "bg-blue-500" },
+      bgColor: 'blue', // Set your default background color
+      hoverColor: 'red', // Set your default hover color
     };
   },
   methods: {
