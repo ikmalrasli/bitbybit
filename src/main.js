eae0c8cebcbeee0c32e8d946fb11c27b57a641ed
ikmalrasli <ikmalrasli@email.com>
2026-02-23 00:24:05 +0800

    offline caching for Firebase Storage images

--- src/main.js
+++ src/main.js
@@ -71,13 +71,14 @@ Sentry.init({
 
 app.mount('#app');
 
-// if ('serviceWorker' in navigator) {
-//     navigator.serviceWorker
-//         .register('firebase-messaging-sw.js')
-//         .then((registration) => {
-//         console.log('Service Worker registered successfully:', registration);
-//         })
-//         .catch((error) => {
-//         console.error('Service Worker registration failed:', error);
-//         });
-//     }
+// Register service worker for PWA functionality and offline caching
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+        .register('firebase-messaging-combined.js')
+        .then((registration) => {
+        console.log('Service Worker registered successfully:', registration);
+        })
+        .catch((error) => {
+        console.error('Service Worker registration failed:', error);
+        });
+    }
