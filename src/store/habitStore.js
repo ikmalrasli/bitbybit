dce87ba506d1ece1205e3e2928f3191afba5c3b4
ikmalrasli <ikmalrasli@email.com>
2026-03-06 11:50:14 +0800

    updated habitStore to use updated function from habitService

--- src/store/habitStore.js
+++ src/store/habitStore.js
@@ -1,19 +1,22 @@
 import { defineStore } from 'pinia';
 import { habitService } from '../services/habitService';
+import { useUserStore } from '../store/userStore';
 
 export const useHabitStore = defineStore('habitStore', {
   state: () => ({
     dayHabits: [],
     selectedDate: new Date(),
+    activeHabitsByDay: {},
+    weekProgress: {}
   }),
   actions: {
-    async loadDayHabits() {
-      // The store just calls the service
-      this.dayHabits = await habitService.getHabitsByDate(this.selectedDate);
-    },
-    async completeHabit(habitId) {
-      await habitService.updateProgress(habitId, 100, this.selectedDate);
-      await this.loadDayHabits(); // Refresh local state
+    async fetchWeek(start, end) {
+      const userStore = useUserStore();
+      const uid = userStore.getUserId;
+      const { calendarMap, allProgress } = await habitService.getWeeklyData(uid, start, end);
+      this.activeHabitsByDay = calendarMap; // Keyed by date "2026-03-05"
+      this.weekProgress = allProgress;
     }
+
   }
 });
