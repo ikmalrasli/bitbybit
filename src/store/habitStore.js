9d45520d471e8a1d3c8882e6f2f05d6784c17506
ikmalrasli <ikmalrasli@email.com>
2026-03-07 01:49:33 +0800

    finalize getHabitMetrics to utilize habitService function

--- src/store/habitStore.js
+++ src/store/habitStore.js
@@ -4,18 +4,17 @@ import { useUserStore } from '../store/userStore';
 
 export const useHabitStore = defineStore('habitStore', {
   state: () => ({
-    dayHabits: [],
     selectedDate: new Date(),
-    activeHabitsByDay: {},
-    weekProgress: {}
+    dayHabitMetrics: {}
   }),
   actions: {
-    async fetchWeek(start, end) {
+    setSelectedDate(date) {
+      this.selectedDate = date;
+    },
+    async getHabitMetrics(start, end) {
       const userStore = useUserStore();
       const uid = userStore.getUserId;
-      const { calendarMap, allProgress } = await habitService.getWeeklyData(uid, start, end);
-      this.activeHabitsByDay = calendarMap; // Keyed by date "2026-03-05"
-      this.weekProgress = allProgress;
+      this.dayHabitMetrics = { ...this.dayHabitMetrics, ...await habitService.fetchHabitMetrics(uid, start, end) };
     }
 
   }
